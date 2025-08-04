a90cfd0e1728f301a4ed8de7ad9bd52bfb425947
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-04 17:59:53 +0300

    I have made a mini-project: Static-File-Server where I have combined http, fs and path modules all together

---
+++ Static-File-Server.js
@@ -0,0 +1,54 @@
+// 🧪 Mini Project: Static File Server
+// Goal: Combine http, fs, and path modules
+// Task:
+//     Create a Node.js server that serves any static file (HTML, CSS, JS) from a public folder.
+//     When a user visits /, serve index.html.
+//     When they visit /about, serve about.html.
+//     Add support for /styles.css, /script.js, etc.
+
+
+import http from 'http';
+import path from 'path';
+import url from 'url';
+import fs from 'fs/promises';
+const PORT = process.env.PORT
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.join(__filename) 
+
+const server = http.createServer(async( req, res) =>{
+    // get method
+    try{
+        if(req.method === 'GET'){
+            let filepath;
+            if(req.url === '/'){
+                filepath = path.join(__dirname, 'public2','index3.html')
+            }else if(req.url === '/about'){
+                filepath = path.join(__dirname, 'public2','about.html')
+            }else if(req.url === '/script'){
+                filepath = path.join(__dirname, 'public2','script.js')
+            }else if(req.url === '/style'){
+                filepath = path.join(__dirname, 'public2','style.css')
+            }else{
+                throw new error('404 Page Not Found')
+            }
+
+
+            const data = await fs.readFile(filepath)
+            res.setHeader('Content-type', 'text/html')
+            res.write(data)
+            res.end()
+
+        }else{
+            throw new Error('Method not Accessible')
+        }
+    }catch(error){
+        res.writeHead(500, {'Content-type' : 'text/html'})
+        res.end('Server error')
+    }
+})
+
+
+server.listen(PORT, () =>{
+    console.log(`Server is running on ${PORT}`)
+})
