d1eeec5f35ea5d584c2cf8ea4bc4b0a771e6eb46
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-04 15:46:30 +0300

    Going through how to create servers in Node JS, one of the very core concepts to learn

---
+++ server3.js
@@ -0,0 +1,10 @@
+import http from 'http';
+import url from 'url';
+import path from 'path';
+import fs from 'fs/promises';
+
+const PORT = process.env.PORT
+
+const server = http.createServer((req, res) =>{
+    
+})
