f20addc06faed63f049a6d88cbc04f951c6b11a2
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-05 06:03:52 +0300

    Corrected a few errors that were causing some problems in my server that is Static-File-server

--- Static-File-Server.js
+++ Static-File-Server.js
@@ -13,29 +13,34 @@ import url from 'url';
 import fs from 'fs/promises';
 const PORT = process.env.PORT
 
+
+// getting current path
 const __filename = url.fileURLToPath(import.meta.url)
-const __dirname = path.join(__filename) 
+const __dirname = path.dirname(__filename) 
 
 const server = http.createServer(async( req, res) =>{
     // get method
     try{
         if(req.method === 'GET'){
             let filepath;
+            let contentType = 'text/html'; 
             if(req.url === '/'){
                 filepath = path.join(__dirname, 'public2','index3.html')
             }else if(req.url === '/about'){
-                filepath = path.join(__dirname, 'public2','about.html')
-            }else if(req.url === '/script'){
+                filepath = path.join(__dirname, 'public2','about2.html')
+            }else if(req.url === '/script.js'){
                 filepath = path.join(__dirname, 'public2','script.js')
-            }else if(req.url === '/style'){
+                contentType = 'application/javascript'
+            }else if(req.url === '/styles.css'){
                 filepath = path.join(__dirname, 'public2','style.css')
+                contentType ='text/css' 
             }else{
                 throw new error('404 Page Not Found')
             }
 
 
             const data = await fs.readFile(filepath)
-            res.setHeader('Content-type', 'text/html')
+            res.setHeader('Content-type', contentType)
             res.write(data)
             res.end()
 
